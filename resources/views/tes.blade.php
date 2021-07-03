8bcf4148b1f667d0921507dbbbf50fb33aafecfe
Ecang <sandytesar@gmail.com>
2021-07-03 14:15:05 +0700

    coba

--- resources/views/tes.blade.php
+++ resources/views/tes.blade.php
@@ -22,10 +22,10 @@
 </body>
 
 <!-- <script src="http://dukuhwaru.tegalkab.go.id/assets/Frontend/page/geoJs.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <script>
     $(document).ready(function() {
-
 
         var statesData = {
             "type": "FeatureCollection",
